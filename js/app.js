b3966dbbd4eba36d874760c3f423d8b775d51ca7
ErnestX <xiangspecial2012@gmail.com>
2023-03-31 01:54:11 -0400

    add mouse orbit control

--- js/app.js
+++ js/app.js
@@ -1,69 +1,50 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// import { AppState } from "./appState.js";
-// import { World } from "./world.js";
-
-// AppState.width = 800;
-// AppState.height = 600;
-
-document.getElementById('MiniMap').style.width = 800; //AppState.width.toString().concat("px");
-document.getElementById('MiniMap').style.height = 600; //AppState.height.toString().concat("px");
+document.getElementById('MiniMap').style.width = 800; 
+document.getElementById('MiniMap').style.height = 600; 
 
 console.log("hello world");
 
-// init
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
+// set up renderer, scene and camera
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
 document.body.appendChild( renderer.domElement );
 
-// animation
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-function animation( time ) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+// set up control 
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.addEventListener( 'change', render );
 
-	renderer.render( scene, camera );
+const geometry1 = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
+const material1 = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
 
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+// geometry with some vertices
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+renderer.render( scene, camera );
+
+function render() {
+  renderer.render( scene, camera );
 }
-
-
-
-// const world = new World(AppState.width, AppState.height, AppState.numOfMountainsToGenerate);
-// world.render();
-
-// document.addEventListener("keydown", function(event) {
-//   const key = event.key; // Or const {key} = event; in ES6+
-//   switch (key) {
-//     case "Escape":
-//       world.unselect(); 
-//       AppState.textBuffer = "";
-//       break;
-//     case "Enter":
-//       AppState.selectedEpisode.text = AppState.textBuffer;
-//       AppState.selectedEpisode.refreshText();
-//       AppState.textBuffer = "";
-//       break;
-//     case "Shift":
-//       break;
-//     default:
-//       AppState.textBuffer = AppState.textBuffer.concat(key);
-//       AppState.selectedEpisode.text = AppState.textBuffer;
-//       AppState.selectedEpisode.refreshText();
-//       console.log("other key pressed");
-//       console.log(AppState.textBuffer);
-//   }
-// });
